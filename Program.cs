3bc4fdfbc60cf5e08a6f55837b9b03e9c153f5ae
Annu Gill <annugill005@gmail.com>
2026-02-13 09:56:54 +0530

    Branch 2 created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+
+namespace DummyGit
+{
+       internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // This is branch2
+            Console.WriteLine("This is branch2");
+        }
+    }
+
+}
